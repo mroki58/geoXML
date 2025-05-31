ee4a2406d1a97e2ab41bf4361e86eed77fa4c78f
Igor Sala <sala-igor@wp.pl>
2025-05-31 23:52:34 +0200

    Stworzenie planowanych endpointów - plany na następne 1. stworzyć endpoint o tworzeniu XML + UDF 2. endpoint do pliku cozrobic.txt 3. Frontendzik

--- expressApplication/routes/deposit.js
+++ expressApplication/routes/deposit.js
@@ -2,64 +2,106 @@ var express = require('express');
 var router = express.Router();
 var axios = require('axios');
 
+// Funkcja pomocnicza do budowania pełnego URL
+function buildCSharpUrl(path) {
+  const CSharpURL = 'http://localhost:5032/xml';
+  return CSharpURL + path;
+}
+
+// PATCH pomocnicza
+async function patchXmlNode(path, res) {
+  try {
+    const url = buildCSharpUrl(path);
+    const ans = await axios.patch(url);
+    res.status(200).send(ans.data);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Błąd przy połączeniu z zewnętrznym API');
+  }
+}
+
+// GET pomocnicza
+async function fetchFromBackend(path, res) {
+  try {
+    const url = buildCSharpUrl(path);
+    const ans = await axios.get(url);
+    res.status(200).send(ans.data);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Błąd przy połączeniu z zewnętrznym API');
+  }
+}
+
+// pomocnicza DELETE
+async function deleteFromBackend(path, res) {
+  try {
+    const url = buildCSharpUrl(path);
+    const ans = await axios.delete(url);
+    res.status(200).send(ans.data);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Błąd przy połączeniu z zewnętrznym API');
+  }
+}
+
+// PATCH endpoints
 router.patch('/latitude/:id', async function(req, res) {
-  try {
-    const { latitude } = req.body;
-    const id = req.params.id;
-    const ans = await axios.patch('http://localhost:5032/xml/node/' + id + '?path=/deposit/gegraphy/latitude&value=' + latitude);
+  const { latitude } = req.body;
+  const id = req.params.id;
+  const path = `/node/${id}?path=/deposit/geography/latitude&value=${encodeURIComponent(latitude)}`;
+  await patchXmlNode(path, res);
+});
 
-    res.status(200).send(ans.data);
-
-  } catch (err) {
-    console.error(err.message);
-    res.status(500).send('Błąd przy połączeniu z zewnętrznym API');
-  }
-
-})
 router.patch('/longitude/:id', async function(req, res) {
-  try {
-    const { longitude } = req.body;
-    const id = req.params.id;
-    const ans = await axios.patch('http://localhost:5032/xml/node/' + id + '?path=/deposit/geography/longitude&value=' + longitude);
+  const { longitude } = req.body;
+  const id = req.params.id;
+  const path = `/node/${id}?path=/deposit/geography/longitude&value=${encodeURIComponent(longitude)}`;
+  await patchXmlNode(path, res);
+});
 
-    res.status(200).send(ans.data);
+router.patch('/radius/:id', async function(req, res) {
+  const { radius } = req.body;
+  const id = req.params.id;
+  const path = `/node/${id}?path=/deposit/geography/radius&value=${encodeURIComponent(radius)}`;
+  await patchXmlNode(path, res);
+});
 
-  } catch (err) {
-    console.error(err.message);
-    res.status(500).send('Błąd przy połączeniu z zewnętrznym API');
-  }
+router.patch('/depth/:id', async function(req, res) {
+  const { depth } = req.body;
+  const id = req.params.id;
+  const path = `/node/${id}?path=/deposit/geology/depth&value=${encodeURIComponent(depth)}`;
+  await patchXmlNode(path, res);
+});
 
+router.patch('/name/:id', async function(req, res) {
+  const { name } = req.body;
+  const id = req.params.id;
+  const path = `/attr/${id}?path=/deposit&value=${encodeURIComponent(name)}&attr=name`;
+  await patchXmlNode(path, res);
+});
+
+
+///////////////////////////////////////////////////////////////
+
+router.delete('/quantity', async function(req, res) {
+  const { nodeName, maxValue } = req.query;
+  const path = `/less?nodeName=${encodeURIComponent(nodeName)}&maxValue=${maxValue}`;
+  await deleteFromBackend(path, res);
+});
+
+// Przykład DELETE endpointu
+router.delete('/name', async function(req, res) {
+  const { nodeName, attrName, attrValue } = req.query;
+  const path = `/attr?nodeName=${encodeURIComponent(nodeName)}&attrName=${encodeURIComponent(attrName)}&attrValue=${encodeURIComponent(attrValue)}`;
+  await deleteFromBackend(path, res);
+});
+
+router.delete('/region', async function(req, res) {
+  const {nodeName, nodeValue} = req.query
+  const path = `?nodeName=${nodeName}&nodeValue=${nodeValue}`
+  await deleteFromBackend(path, res)
 })
 
-
-router.patch('/radius', async function(req, res) {
-  try {
-    const { radius } = req.body;
-    const id = req.params.id;
-    const ans = await axios.patch('http://localhost:5032/xml/node/' + id + '?path=/deposit/geography/latitude&value=' + radius);
-
-    res.status(200).send(ans.data);
-
-  } catch (err) {
-    console.error(err.message);
-    res.status(500).send('Błąd przy połączeniu z zewnętrznym API');
-  }
-
-})
-
-router.patch('/name', async function(req, res) {
-  try {
-    const { name } = req.body;
-    const id = req.params.id;
-    const ans = await axios.patch('http://localhost:5032/xml/attr/' + id + '?path=/deposit&value=' + name + '&attr=name')
-
-    res.status(200).send(ans.data);
-
-  } catch (err) {
-    console.error(err.message);
-    res.status(500).send('Błąd przy połączeniu z zewnętrznym API');
-  }
-
-})
+router.delete('/')
 
 module.exports = router;
