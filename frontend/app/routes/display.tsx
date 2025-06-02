53d8e125b3514405f531b25b6f80a3d749289496
Igor Sala <sala-igor@wp.pl>
2025-06-02 17:59:32 +0200

    Pobieranie danych do mapy z backendu działa

--- frontend/app/routes/display.tsx
+++ frontend/app/routes/display.tsx
@@ -9,11 +9,17 @@ export function meta({}: Route.MetaArgs) {
   ];
 }
 
-export function clientLoader() {
-    return {
-          "0": "<deposit name=\"zloze miedzi A\"><geology><type>rudy miedzi</type><estimatedVolume>1.2 mln ton</estimatedVolume><depth>300 m</depth><status>eksploatowane</status></geology><geography><location>Sudety, Polska</location><region>Dolnośląskie</region><latitude>50.3001</latitude><longitude>16.4375</longitude><radius>5</radius></geography></deposit>",
-          "1": "<deposit name=\"zloze wegla B\"><geology><type>węgiel kamienny</type><estimatedVolume>5 mln ton</estimatedVolume><depth>800 m</depth><status>nieeksploatowane</status></geology><geography><location>Górny Śląsk</location><region>Śląskie</region><latitude>50.2887</latitude><longitude>18.6782</longitude><radius>8</radius></geography></deposit>"
-        }
+export async function clientLoader() {
+    const res = await fetch('/api/all')
+    const data = await res.json();
+
+    let xmlMap = data.data
+
+    Object.keys(xmlMap).map((key : string) => {
+      xmlMap[key] = xmlMap[key][0]
+    })
+
+    return xmlMap
 }
 
 export default function Home({loaderData}: Route.ComponentProps) {
